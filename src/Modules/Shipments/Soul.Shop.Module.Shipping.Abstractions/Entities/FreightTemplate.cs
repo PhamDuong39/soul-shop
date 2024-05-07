d335e9ae76870711d9344cdc62d49b5f63710803
ITE <thaibdtg16@gmail.com>
2024-05-07 14:50:31 +0700

    #6 base-shipping module

---
+++ src/Modules/Shipments/Soul.Shop.Module.Shipping.Abstractions/Entities/FreightTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Soul.Shop.Infrastructure.Models;
+
+namespace Soul.Shop.Module.Shipping.Abstractions.Entities;
+
+public class FreightTemplate : EntityBase
+{
+    public FreightTemplate()
+    {
+        CreatedOn = DateTime.Now;
+        UpdatedOn = DateTime.Now;
+    }
+
+    [StringLength(450)] public string Name { get; set; }
+
+    public string Note { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime UpdatedOn { get; set; }
+
+    public IList<PriceAndDestination> PriceAndDestinations { get; set; } = new List<PriceAndDestination>();
+}
