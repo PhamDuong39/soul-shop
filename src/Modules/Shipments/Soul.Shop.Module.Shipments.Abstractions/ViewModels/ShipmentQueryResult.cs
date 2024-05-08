c0e09073c4749b59fd7fecef622e6a4c0eabbeaa
ITE <thaibdtg16@gmail.com>
2024-05-08 16:58:34 +0700

    SOUL-60 upgrade model base

---
+++ src/Modules/Shipments/Soul.Shop.Module.Shipments.Abstractions/ViewModels/ShipmentQueryResult.cs
@@ -0,0 +1,28 @@
+﻿namespace Soul.Shop.Module.Shipments.Abstractions.ViewModels;
+
+public class ShipmentQueryResult
+{
+    public int Id { get; set; }
+
+    public int OrderId { get; set; }
+
+    public string OrderNo { get; set; }
+    //bug: should be OrderNumber
+    // public Order Status OrderStatus { get; set; }
+    //bug: should be OrderStatus?
+    // public ShippingStatus? ShippingStatus { get; set; }
+
+    public string TrackingNumber { get; set; }
+
+    public decimal TotalWeight { get; set; }
+
+    public DateTime? ShippedOn { get; set; }
+
+    public DateTime? DeliveredOn { get; set; }
+
+    public string AdminComment { get; set; }
+
+    public string CreatedBy { get; set; }
+
+    public IList<ShipmentQueryItemResult> Items { get; set; } = new List<ShipmentQueryItemResult>();
+}
---
+++ src/Modules/Shipments/Soul.Shop.Module.Shipping/Data/ShippingCustomModelBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soul.Shop.Infrastructure.Data;
+using Soul.Shop.Module.Shipping.Abstractions.Entities;
+
+namespace Soul.Shop.Module.Shipping.Data;
+
+public class ShippingCustomModelBuilder : ICustomModelBuilder
+{
+    public void Build(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FreightTemplate>().HasQueryFilter(c => !c.IsDeleted);
+        modelBuilder.Entity<PriceAndDestination>().HasQueryFilter(c => !c.IsDeleted);
+    }
+}
