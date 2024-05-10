8c3421b9bde7395f2ecb8aaf76306e9b3cd29242
ITE <thaibdtg16@gmail.com>
2024-05-10 15:47:18 +0700

    :coffin: SOUL-69 remove code in log

--- src/Modules/Email/Soul.Shop.Module.EmailSenderSmtp/EmailSender.cs
+++ src/Modules/Email/Soul.Shop.Module.EmailSenderSmtp/EmailSender.cs
@@ -59,13 +59,13 @@ public class EmailSender(
             }
 
             send.IsSucceed = true;
-            logger.LogInformation("Mail sent successfully", send);
+            logger.LogInformation("Mail sent successfully");
         }
         catch (Exception ex)
         {
             send.Message = ex.Message;
             send.IsSucceed = false;
-            logger.LogError(ex, "Email sending exception", send, email, subject, body, isHtml);
+            logger.LogError(ex, "Email sending exception");
         }
         finally
         {
