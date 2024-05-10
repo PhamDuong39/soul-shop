6016e932b116eb8e3c35feef00ae0460db4a4625
ITE <thaibdtg16@gmail.com>
2024-05-10 17:11:57 +0700

    :white_check_mark: SOUL-70 add sms module

---
+++ src/Modules/Core/Soul.Shop.Module.Core.Abstractions/Models/SmsTemplateType.cs
@@ -0,0 +1,7 @@
+﻿namespace Soul.Shop.Module.Core.Abstractions.Models;
+
+public enum SmsTemplateType
+{
+    Captcha = 0,
+    Notification = 1
+}
---
+++ src/Modules/Soul.Shop.Module.SmsSender/Models/SpeedSenderSmsOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Soul.Shop.Module.SmsSender.Models;
+
+public class SpeedSenderSmsOptions
+{
+
+    public string RegionId { get; set; }
+
+    public string AccessKeyId { get; set; }
+
+    public string AccessKeySecret { get; set; }
+
+    public bool IsTest { get; set; }
+}
