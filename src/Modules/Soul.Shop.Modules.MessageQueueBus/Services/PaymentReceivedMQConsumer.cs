4b164df87a0cd2d8064a23c5dbbadf46c874a63b
eliasbui <buithai16102003@gmail.com>
2024-05-12 21:51:16 +0700

    :white_check_mark: SOUL-71 add modules message queue

---
+++ src/Modules/Soul.Shop.Modules.MessageQueueBus/Services/PaymentReceivedMQConsumer.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Soul.Shop.Module.Orders.Abstractions.Events;
+
+namespace Soul.Shop.Modules.MessageQueueBus.Services
+{
+    public class PaymentReceivedMQConsumer(
+        ILogger<PaymentReceivedMQConsumer> logger,
+        IMediator mediator)
+        : IConsumer<PaymentReceived>
+    {
+        private readonly ILogger _logger = logger;
+
+        public async Task Consume(ConsumeContext<PaymentReceived> context)
+        {
+            try
+            {
+                if (context?.Message != null)
+                {
+                    await mediator.Publish(context.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Processing failed", context?.Message);
+            }
+        }
+    }
+}
