d26959fbc49f2ab9aa896f4dd431f1ebdbf002dd
ITE <thaibdtg16@gmail.com>
2024-05-13 14:50:03 +0700

    :white_check_mark: SOUL-67 add project base auth

---
+++ src/Modules/Auth/ApiProfiler/Soul.Shop.Module.ApiProfiler/Internal/MiniProfilerBaseOptionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Soul.Shop.Module.ApiProfiler.Internal;
+
+public static class MiniProfilerBaseOptionsExtensions
+{
+    public static List<Guid> ExpireAndGetUnviewed(this MiniProfilerBaseOptions options, string user)
+    {
+        var ids = options.Storage?.GetUnviewedIds(user);
+        if (!(ids?.Count > options.MaxUnviewedProfiles)) return ids;
+        for (var i = 0; i < ids.Count - options.MaxUnviewedProfiles; i++)
+            options.Storage.SetViewedAsync(user, ids[i]);
+        return ids;
+    }
+
+    public static async Task<List<Guid>> ExpireAndGetUnviewedAsync(this MiniProfilerBaseOptions options, string user)
+    {
+        if (options.Storage == null) return null;
+        var ids = await options.Storage.GetUnviewedIdsAsync(user).ConfigureAwait(false);
+        if (!(ids?.Count > options.MaxUnviewedProfiles)) return ids;
+        for (var i = 0; i < ids.Count - options.MaxUnviewedProfiles; i++)
+            await options.Storage.SetViewedAsync(user, ids[i]).ConfigureAwait(false);
+        return ids;
+    }
+}
---
+++ src/Modules/Auth/ApiProfiler/Soul.Shop.Module.ApiProfiler/Suppression.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.Serialization;
+
+namespace Soul.Shop.Module.ApiProfiler;
+
+/// <summary>
+/// An individual suppression block that deactivates profiling temporarily
+/// </summary>
+[DataContract]
+public class Suppression : IDisposable
+{
+    private readonly bool _wasSuppressed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Suppression"/> class. 
+    /// Obsolete - used for serialization.
+    /// </summary>
+    [Obsolete("Used for serialization")]
+    public Suppression()
+    {
+        /* serialization only */
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Suppression"/> class. 
+    /// Creates a new Suppression to deactivate profiling while alive
+    /// </summary>
+    /// <param name="profiler">The <see cref="Soul.Shop.Module.ApiProfiler.MiniProfiler"/> to suppress.</param>
+    /// <exception cref="ArgumentNullException">Throws when the <paramref name="profiler"/> is <c>null</c>.</exception>
+    public Suppression(Soul.Shop.Module.ApiProfiler.MiniProfiler profiler)
+    {
+        Profiler = profiler ?? throw new ArgumentNullException(nameof(profiler));
+        if (!Profiler.IsActive) return;
+
+        Profiler.IsActive = false;
+        _wasSuppressed = true;
+    }
+
+    /// <summary>
+    /// Gets a reference to the containing profiler, allowing this Suppression to affect profiler activity.
+    /// </summary>
+    internal Soul.Shop.Module.ApiProfiler.MiniProfiler Profiler { get; }
+
+    void IDisposable.Dispose()
+    {
+        if (Profiler != null && _wasSuppressed) Profiler.IsActive = true;
+    }
+}
