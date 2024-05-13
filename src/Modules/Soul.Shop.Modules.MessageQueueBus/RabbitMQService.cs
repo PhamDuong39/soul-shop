f8ea5a6a3572215e656022015f64fbf92da29019
ITE <thaibdtg16@gmail.com>
2024-05-13 10:45:00 +0700

    :bug: SOUL-79 change constructor add private value

--- src/Modules/Soul.Shop.Modules.MessageQueueBus/RabbitMQService.cs
+++ src/Modules/Soul.Shop.Modules.MessageQueueBus/RabbitMQService.cs
@@ -1,14 +1,15 @@
 ﻿using MassTransit;
 using Soul.Shop.Modules.MessageQueueBus.Abstractions;
 
-namespace Soul.Shop.Modules.MessageQueueBus
+namespace Soul.Shop.Modules.MessageQueueBus;
+
+public class RabbitMQService(IBusControl busControl) : IMQService
 {
-    public class RabbitMQService(IBusControl busControl) : IMQService
+    private readonly IBusControl _busControl = busControl;
+
+    public async Task Send<T>(string queue, T message) where T : class
     {
-        public async Task Send<T>(string queue, T message) where T : class
-        {
-            var sendEndpoint = await busControl.GetSendEndpoint(new Uri($"queue:{queue}"));
-            await sendEndpoint.Send(message);
-        }
+        var sendEndpoint = await _busControl.GetSendEndpoint(new Uri($"queue:{queue}"));
+        await sendEndpoint.Send(message);
     }
 }
