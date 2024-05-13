c38e5e288d50d7347d6c76ef9cc76ef3c38c52f0
ITE <thaibdtg16@gmail.com>
2024-05-13 10:37:44 +0700

    :white_check_mark: SOUL-73 add schedule base modules

---
+++ src/Modules/Schedule/Soul.Shop.Module.Schedule.Abstractions/IJobService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Soul.Shop.Module.Schedule.Abstractions;
+
+public interface IJobService
+{
+    Task<string> Enqueue(Expression<Func<Task>> methodCall);
+    Task<string> Schedule(Expression<Func<Task>> methodCall, TimeSpan timeSpan);
+    Task AddOrUpdate(string recurringJobId, Expression<Func<Task>> methodCall, Func<string> cronExpression);
+}
