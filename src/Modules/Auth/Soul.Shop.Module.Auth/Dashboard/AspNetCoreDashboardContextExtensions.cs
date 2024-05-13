4bb9b565f05502d713ac9d37fc2b87e6c76abed6
ITE <thaibdtg16@gmail.com>
2024-05-13 13:43:33 +0700

    :white_check_mark: SOUL-67 add basic author support

---
+++ src/Modules/Auth/Soul.Shop.Module.Auth/Dashboard/AspNetCoreDashboardContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Soul.Shop.Module.Auth.Dashboard;
+
+public static class AspNetCoreDashboardContextExtensions
+{
+    public static HttpContext GetHttpContext(this DashboardContext context)
+    {
+        if (context == null) throw new ArgumentNullException(nameof(context));
+
+        var aspNetCoreContext = context as AspNetCoreDashboardContext;
+        if (aspNetCoreContext == null)
+            throw new ArgumentException($"Context argument should be of type `{nameof(AspNetCoreDashboardContext)}`!",
+                nameof(context));
+
+        return aspNetCoreContext.HttpContext;
+    }
+}
