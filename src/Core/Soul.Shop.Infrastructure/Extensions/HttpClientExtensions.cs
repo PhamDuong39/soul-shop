17d9dcc3166af8c4172260bbca7c69ef9a2b1355
eliasbui <buithai16102003@gmail.com>
2024-05-18 18:53:39 +0700

    :sparkles: chore: introduction new device (linux config git)

--- src/Core/Soul.Shop.Infrastructure/Extensions/HttpClientExtensions.cs
+++ src/Core/Soul.Shop.Infrastructure/Extensions/HttpClientExtensions.cs
@@ -4,14 +4,14 @@ namespace Soul.Shop.Infrastructure.Extensions;
 
 public static class HttpClientExtensions
 {
-    private static readonly JsonSerializer JsonSerializer = new();
+    private static readonly JsonSerializer s_jsonSerializer = new();
 
     public static async Task<T?> ReadAsJsonAsync<T>(this HttpContent httpContent)
     {
         await using var stream = await httpContent.ReadAsStreamAsync();
         var jsonReader = new JsonTextReader(new StreamReader(stream));
 
-        return JsonSerializer.Deserialize<T>(jsonReader);
+        return s_jsonSerializer.Deserialize<T>(jsonReader);
     }
 
     public static Task<HttpResponseMessage> PostJsonAsync<T>(this HttpClient client, string url, T value)
@@ -29,7 +29,7 @@ public static class HttpClientExtensions
     {
         var stream = new MemoryStream();
         var jsonWriter = new JsonTextWriter(new StreamWriter(stream));
-        JsonSerializer.Serialize(jsonWriter, value);
+        s_jsonSerializer.Serialize(jsonWriter, value);
         jsonWriter.Flush();
         stream.Position = 0;
         var request = new HttpRequestMessage(method, url)
