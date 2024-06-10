bdbd85c5b671ffbb73d2c2ba3ebc34b5a34fd2d7
Phạm Ánh Dương <phamanhduong30092003@gmail.com>
2024-06-10 10:38:54 +0700

    :safety_vest: feat: update entities

---
+++ src/Modules/Soul.Shop.Module.Support.Abstractions/Entities/Discounts.cs
@@ -0,0 +1,56 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Soul.Shop.Module.Support.Abstractions.Entities;
+
+public class Discount
+{
+    public int DiscountID
+    {
+        get;
+        set;
+    }
+
+    public string DiscountCode
+    {
+        get;
+        set;
+    }
+
+    public string Description
+    {
+        get;
+        set;
+    }
+
+    public decimal DiscountAmount
+    {
+        get;
+        set;
+    }
+
+    public DateTime StartDate
+    {
+        get;
+        set;
+    }
+
+    public DateTime EndDate
+    {
+        get;
+        set;
+    }
+
+    public decimal MinOrderAmount
+    {
+        get;
+        set;
+    }
+
+    public int MaxUses
+    {
+        get;
+        set;
+    }
+}
